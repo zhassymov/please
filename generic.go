4abc521e33608c70ba7bc8ea765668e444e0441f
Arthur Zhassymov <zhassymov>
2024-05-21 11:42:57 +0500

    feat: Validade.WithError

--- generic.go
+++ generic.go
@@ -1,3 +1,12 @@
 package please
 
 type Validate[T any] func(T) error
+
+func (v Validate[T]) WithError(cause error) Validate[T] {
+	return func(value T) error {
+		if err := v(value); err != nil {
+			return cause
+		}
+		return nil
+	}
+}
