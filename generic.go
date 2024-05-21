254f3f3479128310f0b5a6d15debda0336a48172
Arthur Zhassymov <zhassymov>
2024-05-21 11:44:14 +0500

    feat: Collect

--- generic.go
+++ generic.go
@@ -19,3 +19,19 @@ func Abort[T any](value T, vs ...Validate[T]) error {
 	}
 	return nil
 }
+
+func Collect[T any](value T, vs ...Validate[T]) []error {
+	if len(vs) == 0 {
+		return nil
+	}
+	errs := make([]error, 0, len(vs))
+	for _, v := range vs {
+		if err := v(value); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	if len(errs) == 0 {
+		return nil
+	}
+	return errs
+}
