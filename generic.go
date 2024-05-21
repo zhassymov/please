b6437351d5c6f6b35657c36e36d1aa17fa78045a
Arthur Zhassymov <zhassymov>
2024-05-21 11:43:44 +0500

    feat: Abort

--- generic.go
+++ generic.go
@@ -10,3 +10,12 @@ func (v Validate[T]) WithError(cause error) Validate[T] {
 		return nil
 	}
 }
+
+func Abort[T any](value T, vs ...Validate[T]) error {
+	for _, v := range vs {
+		if err := v(value); err != nil {
+			return err
+		}
+	}
+	return nil
+}
