07fcb72c67cd95efeb1c440cbc09ff13da1818d6
Arthur Zhassymov <zhassymov>
2024-07-02 09:25:04 +0500

    fix: ordered

--- ordered.go
+++ ordered.go
@@ -5,37 +5,41 @@ import (
 	"fmt"
 )
 
-func Min[T cmp.Ordered](min T) Validate[T] {
+func Min[T cmp.Ordered](minimal T) Validate[T] {
 	return func(value T) error {
-		if value < min {
-			return fmt.Errorf("%v must be greater or equal than %v", value, min)
+		if value < minimal {
+			return fmt.Errorf("%v must be greater or equal than %v", value, minimal)
 		}
 		return nil
 	}
 }
 
-func Max[T cmp.Ordered](max T) Validate[T] {
+func Max[T cmp.Ordered](maximal T) Validate[T] {
 	return func(value T) error {
-		if value > max {
-			return fmt.Errorf("%v must be less or equal than %v", value, max)
+		if value > maximal {
+			return fmt.Errorf("%v must be less or equal than %v", value, maximal)
 		}
 		return nil
 	}
 }
 
-func Between[T cmp.Ordered](min, max T) Validate[T] {
+func Between[T cmp.Ordered](x, y T) Validate[T] {
 	return func(value T) error {
-		if value < min || value > max {
-			return fmt.Errorf("%v must be between %v and %v", value, min, max)
+		minimal := min(x, y)
+		maximal := max(x, y)
+		if value < minimal || value > maximal {
+			return fmt.Errorf("%v must be between %v and %v", value, minimal, maximal)
 		}
 		return nil
 	}
 }
 
-func NotBetween[T cmp.Ordered](min, max T) Validate[T] {
+func NotBetween[T cmp.Ordered](x, y T) Validate[T] {
 	return func(value T) error {
-		if value >= min && value <= max {
-			return fmt.Errorf("%v must not be between %v and %v", value, min, max)
+		minimal := min(x, y)
+		maximal := max(x, y)
+		if value >= minimal && value <= maximal {
+			return fmt.Errorf("%v must not be between %v and %v", value, minimal, maximal)
 		}
 		return nil
 	}
