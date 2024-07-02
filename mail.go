6ca28e504c6a4550118ebfb6fac6490cdc264e35
Arthur Zhassymov <zhassymov>
2024-07-02 10:28:31 +0500

    docs: update email

--- mail.go
+++ mail.go
@@ -2,7 +2,7 @@ package please
 
 import "net/mail"
 
-// Email returns a validation function for email address.
+// Email returns a validation function that checks whether the string is a valid email address.
 func Email() Validate[string] {
 	return func(s string) error {
 		_, err := mail.ParseAddress(s)
