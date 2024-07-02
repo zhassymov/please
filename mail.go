f8b7888176eefd71c4b2f47042a9e320359ce4fc
Arthur Zhassymov <zhassymov>
2024-07-02 09:19:10 +0500

    docs: mail

--- mail.go
+++ mail.go
@@ -2,6 +2,7 @@ package please
 
 import "net/mail"
 
+// Email returns a validation function for email address.
 func Email() Validate[string] {
 	return func(s string) error {
 		_, err := mail.ParseAddress(s)
